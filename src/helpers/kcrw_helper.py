be5c951a565af2924ce41d7cc7955e68f125856c
Pablo Grinberg <grinbergpablom@gmail.com>
2024-04-26 13:06:33 -0300

    fix: handle dates

--- src/helpers/kcrw_helper.py
+++ src/helpers/kcrw_helper.py
@@ -1,10 +1,30 @@
 import json
 import requests
+import pytz
+from datetime import datetime, timedelta
 
 base_url = "https://tracklist-api.kcrw.com/Simulcast/date/{year}/{month}/{day}"
 track_id_field = "spotify_id"
 track_search_field = "affiliateLinkSpotify"
 search_prefix = "spotify:search:"
+episode_timezone = pytz.timezone('America/Los_Angeles')
+
+
+def get_last_episode_date():
+    now = datetime.now(episode_timezone)
+    today = now.date()
+    today_weekday = today.weekday()
+    episode_upload = datetime(today.year, today.month, today.day, 13, 0, 0, 0, episode_timezone)
+
+    if today_weekday in [1, 2, 3, 4, 5] and now < episode_upload:
+        today = today - timedelta(days=1)
+    if today_weekday == 6:
+        today = today - timedelta(days=1)
+    if today_weekday == 7:
+        today = today - timedelta(days=2)
+    if today_weekday == 0 and now < episode_upload:
+        today = today - timedelta(days=3)
+    return today
 
 
 def get_tracklist(date):
--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
-from datetime import date
-from helpers.kcrw_helper import get_tracklist
-from helpers.spotify_helper import get_user_data, search_tracks, update_playlist
+from helpers.kcrw_helper import get_last_episode_date, get_tracklist
+from helpers.spotify_helper import get_user_data, get_tracks_ids, update_playlist
 
 if __name__ == '__main__':
-    today = date.today()
 
-    if today.weekday() < 5:
-        tracklist = get_tracklist(today)
-        spotify_client, spotify_user_id = get_user_data()
-        tracks = search_tracks(spotify_client, tracklist)
-        update_playlist(spotify_client, spotify_user_id, tracks)
+    date = get_last_episode_date()
+    tracklist = get_tracklist(date)
+    spotify_client, spotify_user_id = get_user_data()
+    tracks = get_tracks_ids(spotify_client, tracklist)
+    update_playlist(spotify_client, spotify_user_id, tracks)
